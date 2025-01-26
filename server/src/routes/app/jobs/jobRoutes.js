a9c45fa718937a4af0e53a6227e2c4c6e6029abc
Omer Miloh <omerrob11@gmail.com>
2025-01-26 19:28:27 +0200

    feat: adding routes for getting all jobs

--- server/src/routes/app/jobs/jobRoutes.js
+++ server/src/routes/app/jobs/jobRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const jobsRouter = express.Router();
 
 const { validateJobEntry } = require("../../../middleware/jobs/jobsMiddleware");
-const { postJob } = require("../../../controllers/jobsController");
+const { postJob, getJobsList } = require("../../../controllers/jobsController");
 module.exports = jobsRouter;
 
 jobsRouter.post("/", validateJobEntry, postJob);
 
-jobsRouter.get("/");
+jobsRouter.get("/", getJobsList);
 // we need to get jobs
 // what middlewares do we need?
 // lets start with the model then
